976ab90ccc39769e613002a551e8fc2866b03c1e
Pin Lin <moneycat711@gmail.com>
2018-03-22 10:40:31 +0800

    fix JUnit test again

--- app/src/test/java/edu/ntut/user/myhw3/MarriageSuggestionTest.java
+++ app/src/test/java/edu/ntut/user/myhw3/MarriageSuggestionTest.java
@@ -25,29 +25,29 @@ public class MarriageSuggestionTest {
 
     @Test
     public void testGetSuggestion() {
-        assertEquals("建議：趕快結婚", ms.getSuggestion("male", 1, 1));
-        assertEquals("建議：趕快結婚", ms.getSuggestion("male", 1, 2));
-        assertEquals("建議：還不急", ms.getSuggestion("male", 1, 3));
+        assertEquals("建議：趕快結婚", ms.getSuggestion("male", 1, 3));
+        assertEquals("建議：趕快結婚", ms.getSuggestion("male", 1, 7));
+        assertEquals("建議：還不急", ms.getSuggestion("male", 1, 11));
 
-        assertEquals("建議：趕快結婚", ms.getSuggestion("male", 2, 1));
-        assertEquals("建議：開始找對象", ms.getSuggestion("male", 2, 2));
-        assertEquals("建議：還不急", ms.getSuggestion("male", 2, 3));
+        assertEquals("建議：趕快結婚", ms.getSuggestion("male", 2, 3));
+        assertEquals("建議：開始找對象", ms.getSuggestion("male", 2, 7));
+        assertEquals("建議：還不急", ms.getSuggestion("male", 2, 11));
 
-        assertEquals("建議：開始找對象", ms.getSuggestion("male", 3, 1));
-        assertEquals("建議：趕快結婚", ms.getSuggestion("male", 3, 2));
         assertEquals("建議：開始找對象", ms.getSuggestion("male", 3, 3));
+        assertEquals("建議：趕快結婚", ms.getSuggestion("male", 3, 7));
+        assertEquals("建議：開始找對象", ms.getSuggestion("male", 3, 11));
 
         
-        assertEquals("建議：趕快結婚", ms.getSuggestion("female", 1, 1));
-        assertEquals("建議：趕快結婚", ms.getSuggestion("female", 1, 2));
-        assertEquals("建議：還不急", ms.getSuggestion("female", 1, 3));
+        assertEquals("建議：趕快結婚", ms.getSuggestion("female", 1, 3));
+        assertEquals("建議：趕快結婚", ms.getSuggestion("female", 1, 7));
+        assertEquals("建議：還不急", ms.getSuggestion("female", 1, 11));
 
-        assertEquals("建議：趕快結婚", ms.getSuggestion("female", 2, 1));
-        assertEquals("建議：開始找對象", ms.getSuggestion("female", 2, 2));
-        assertEquals("建議：還不急", ms.getSuggestion("female", 2, 3));
+        assertEquals("建議：趕快結婚", ms.getSuggestion("female", 2, 3));
+        assertEquals("建議：開始找對象", ms.getSuggestion("female", 2, 7));
+        assertEquals("建議：還不急", ms.getSuggestion("female", 2, 11));
 
-        assertEquals("建議：開始找對象", ms.getSuggestion("female", 3, 1));
-        assertEquals("建議：趕快結婚", ms.getSuggestion("female", 3, 2));
         assertEquals("建議：開始找對象", ms.getSuggestion("female", 3, 3));
+        assertEquals("建議：趕快結婚", ms.getSuggestion("female", 3, 7));
+        assertEquals("建議：開始找對象", ms.getSuggestion("female", 3, 11));
     }
 }
