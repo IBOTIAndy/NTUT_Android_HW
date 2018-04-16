8144d9b722566027265d15e0093bd07db5cca715
Pin Lin <moneycat711@gmail.com>
2018-04-16 20:22:46 +0800

    convert animation res to java code

--- app/src/main/java/com/galleryusingviewanimation/MainActivity.java
+++ app/src/main/java/com/galleryusingviewanimation/MainActivity.java
@@ -4,7 +4,15 @@ import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
 import android.view.animation.AnimationUtils;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
 import android.widget.AdapterView;
 import android.widget.FrameLayout;
 import android.widget.GridView;
@@ -29,6 +37,7 @@ public class MainActivity extends AppCompatActivity implements ViewSwitcher.View
             R.drawable.img07th, R.drawable.img08th, R.drawable.img09th,
             R.drawable.img10th, R.drawable.img11th, R.drawable.img12th};
 
+    private Animation[] animationList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,9 +54,109 @@ public class MainActivity extends AppCompatActivity implements ViewSwitcher.View
 
         ImageAdapter imgAdap = new ImageAdapter(this, miThumbImgArr);
 
+        initAnimation();
+
         mGridView = (GridView)findViewById(R.id.gridView);
         mGridView.setAdapter(imgAdap);
         mGridView.setOnItemClickListener(gridViewOnItemClick);
+    }
+
+    private void initAnimation() {
+        ScaleAnimation scale;
+        TranslateAnimation trans;
+        RotateAnimation rotate;
+
+        // alphaIn
+        AlphaAnimation alphaIn = new AlphaAnimation(0f, 1f);
+        alphaIn.setInterpolator(new LinearInterpolator());
+        alphaIn.setDuration(1000);
+
+        // alphaOut
+        AlphaAnimation alphaOut = new AlphaAnimation(1f, 0f);
+        alphaOut.setInterpolator(new LinearInterpolator());
+        alphaOut.setDuration(1000);
+
+        // scale_rotate_in
+        AnimationSet scaleRotateIn = new AnimationSet(false);
+        scale = new ScaleAnimation(0f, 1f, 0f, 1f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        scale.setInterpolator(new LinearInterpolator());
+        scale.setStartOffset(1000);
+        scale.setDuration(1000);
+        rotate = new RotateAnimation(0, 3600, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotate.setInterpolator(new AccelerateDecelerateInterpolator());
+        rotate.setStartOffset(1000);
+        rotate.setDuration(1000);
+        scaleRotateIn.addAnimation(scale);
+        scaleRotateIn.addAnimation(rotate);
+
+        // scale_rotate_out
+        AnimationSet scaleRotateOut = new AnimationSet(false);
+        scale = new ScaleAnimation(1f, 0f, 1f, 0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        scale.setInterpolator(new LinearInterpolator());
+        scale.setDuration(1000);
+        rotate = new RotateAnimation(0, 3600, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotate.setInterpolator(new AccelerateDecelerateInterpolator());
+        rotate.setDuration(1000);
+        scaleRotateOut.addAnimation(scale);
+        scaleRotateOut.addAnimation(rotate);
+
+        // scale_rotate_trans_in
+        AnimationSet scaleRotateTransIn = new AnimationSet(false);
+        scale = new ScaleAnimation(0f, 1f, 0f, 1f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        scale.setInterpolator(new LinearInterpolator());
+        scale.setStartOffset(1000);
+        scale.setDuration(1000);
+        rotate = new RotateAnimation(0, 3600, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotate.setInterpolator(new AccelerateDecelerateInterpolator());
+        rotate.setStartOffset(1000);
+        rotate.setDuration(1000);
+        trans = new TranslateAnimation(0, 0, -500, 0);
+        trans.setInterpolator(new LinearInterpolator());
+        trans.setStartOffset(1000);
+        trans.setDuration(1000);
+        scaleRotateTransIn.addAnimation(scale);
+        scaleRotateTransIn.addAnimation(rotate);
+        scaleRotateTransIn.addAnimation(trans);
+
+        // scale_rotate_trans_out
+        AnimationSet scaleRotateTransOut = new AnimationSet(false);
+        scale = new ScaleAnimation(1f, 0f, 1f, 0f, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        scale.setInterpolator(new LinearInterpolator());
+        scale.setDuration(1000);
+        rotate = new RotateAnimation(0, 3600, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        rotate.setInterpolator(new AccelerateDecelerateInterpolator());
+        rotate.setDuration(1000);
+        trans = new TranslateAnimation(0, 0, 0, 500);
+        trans.setInterpolator(new LinearInterpolator());
+        trans.setDuration(1000);
+        scaleRotateTransOut.addAnimation(scale);
+        scaleRotateTransOut.addAnimation(rotate);
+        scaleRotateTransOut.addAnimation(trans);
+
+        // trans_in
+        AnimationSet transIn = new AnimationSet(false);
+        trans = new TranslateAnimation(0, 0, -1000, 0);
+        trans.setInterpolator(new LinearInterpolator());
+        trans.setDuration(1000);
+        transIn.addAnimation(trans);
+
+        // trans_out
+        AnimationSet transOut = new AnimationSet(false);
+        trans = new TranslateAnimation(0, 0, 0, 300);
+        trans.setInterpolator(new LinearInterpolator());
+        trans.setDuration(1000);
+        transOut.addAnimation(trans);
+
+        animationList = new Animation[]{
+                alphaIn,
+                alphaOut,
+                scaleRotateIn,
+                scaleRotateOut,
+                scaleRotateTransIn,
+                scaleRotateTransOut,
+                transIn,
+                transOut
+        };
     }
 
     @Override
@@ -70,32 +179,11 @@ public class MainActivity extends AppCompatActivity implements ViewSwitcher.View
                                         View v,
                                         int position,
                                         long id) {
-                    switch ((int)(Math.random()*4 + 1)) {
-                        case 1:
-                            mImgSwitcher.setInAnimation(AnimationUtils.loadAnimation(MainActivity.this,
-                                    R.anim.alpha_in));
-                            mImgSwitcher.setOutAnimation(AnimationUtils.loadAnimation(MainActivity.this,
-                                    R.anim.alpha_out));
-                            break;
-                        case 2:
-                            mImgSwitcher.setInAnimation(AnimationUtils.loadAnimation(MainActivity.this,
-                                    R.anim.trans_in));
-                            mImgSwitcher.setOutAnimation(AnimationUtils.loadAnimation(MainActivity.this,
-                                    R.anim.trans_out));
-                            break;
-                        case 3:
-                            mImgSwitcher.setInAnimation(AnimationUtils.loadAnimation(MainActivity.this,
-                                    R.anim.scale_rotate_in));
-                            mImgSwitcher.setOutAnimation(AnimationUtils.loadAnimation(MainActivity.this,
-                                    R.anim.scale_rotate_out));
-                            break;
-                        case 4:
-                            mImgSwitcher.setInAnimation(AnimationUtils.loadAnimation(MainActivity.this,
-                                    R.anim.scale_rotate_trans_in));
-                            mImgSwitcher.setOutAnimation(AnimationUtils.loadAnimation(MainActivity.this,
-                                    R.anim.scale_rotate_trans_out));
-                            break;
-                    }
+
+                    int rand = (int)(Math.random() * 4 + 1);
+
+                    mImgSwitcher.setInAnimation(animationList[rand * 2]);
+                    mImgSwitcher.setOutAnimation(animationList[rand * 2 + 1]);
 
                     mImgSwitcher.setImageResource(miImgArr[position]);
                 }
