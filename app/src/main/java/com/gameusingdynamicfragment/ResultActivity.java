88c878fd4c6ab00b1f8a5e926ff07f6ff66a9d5d
Pin Lin <moneycat711@gmail.com>
2018-05-07 01:01:47 +0800

    almost ok ResultActivity

--- app/src/main/java/com/gameusingdynamicfragment/ResultActivity.java
+++ app/src/main/java/com/gameusingdynamicfragment/ResultActivity.java
@@ -1,13 +1,33 @@
 package com.gameusingdynamicfragment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class ResultActivity extends AppCompatActivity {
+
+    private EditText mEdtCountSet,
+            mEdtCountPlayerWin,
+            mEdtCountComWin,
+            mEdtCountDraw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
+
+        mEdtCountSet = (EditText) findViewById(R.id.edtCountSet);
+        mEdtCountPlayerWin = (EditText) findViewById(R.id.edtCountPlayerWin);
+        mEdtCountComWin = (EditText) findViewById(R.id.edtCountComWin);
+        mEdtCountDraw = (EditText) findViewById(R.id.edtCountDraw);
+
+        Intent intent = getIntent();
+
+        mEdtCountSet.setText(String.valueOf(intent.getIntExtra("CountSet", 0)));
+        mEdtCountPlayerWin.setText(String.valueOf(intent.getIntExtra("CountPlayerWin", 0)));
+        mEdtCountComWin.setText(String.valueOf(intent.getIntExtra("CountComWin", 0)));
+        mEdtCountDraw.setText(String.valueOf(intent.getIntExtra("CountDraw", 0)));
+
     }
 }
