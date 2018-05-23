d61dca739c9f7cdf804d762a17b473b043bd9abe
Pin Lin <moneycat711@gmail.com>
2018-05-23 18:31:20 +0800

    prevent double play

--- app/src/main/java/tw/com/ntut/ntut_android_hw/MediaPlayService.java
+++ app/src/main/java/tw/com/ntut/ntut_android_hw/MediaPlayService.java
@@ -4,6 +4,7 @@ import android.app.Service;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.IBinder;
+import android.widget.Toast;
 
 public class MediaPlayService extends Service {
     private MediaPlayer mediaPlayer;
@@ -15,9 +16,23 @@ public class MediaPlayService extends Service {
     }
 
     @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
+    public void onCreate() {
+        super.onCreate();
+
         mediaPlayer = MediaPlayer.create(this, R.raw.china_song);
-        mediaPlayer.start();
+        mediaPlayer.setLooping(true);
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        // 播放中防呆
+        if (mediaPlayer.isPlaying()) {
+            Toast.makeText(this, R.string.music_is_playing, Toast.LENGTH_SHORT).show();
+        }
+        else {
+            mediaPlayer.start();
+        }
+
         return super.onStartCommand(intent, flags, startId);
     }
 
