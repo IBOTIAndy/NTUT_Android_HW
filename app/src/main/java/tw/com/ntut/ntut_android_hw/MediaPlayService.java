1a4a82a58e224c56f8807a88637e842b977f7cf5
Pin Lin <moneycat711@gmail.com>
2018-05-23 14:07:28 +0800

    new service and new song

---
+++ app/src/main/java/tw/com/ntut/ntut_android_hw/MediaPlayService.java
@@ -0,0 +1,16 @@
+package tw.com.ntut.ntut_android_hw;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class MediaPlayService extends Service {
+    public MediaPlayService() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
